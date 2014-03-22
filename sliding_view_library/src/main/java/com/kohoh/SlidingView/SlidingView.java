5c6cb84240f834318df99935b26724c19bc552db
kohoh <qxw2012@hotmail.com>
2014-03-22 21:53:46 +0800

    add some TODO

--- sliding_view_library/src/main/java/com/kohoh/SlidingView/SlidingView.java
+++ sliding_view_library/src/main/java/com/kohoh/SlidingView/SlidingView.java
@@ -1,3 +1,6 @@
+//TODO 更改现有的坐标系
+//TODO slidebound似乎没有必要设置这么多set方法，一个就够了
+//TODO setDrwaingCache似乎需要重构，改成setSlidingCache,并且该View不需要cache。zhe点还可以对SLiingLayer进行修改，也许SlidingView也需要修改。
 package com.kohoh.SlidingView;
 
 import android.content.Context;
